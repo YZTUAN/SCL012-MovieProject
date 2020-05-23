59d6f0b1744be8bf84a62ffba329f67bb0b16190
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2020-05-23 14:52:35 +0000

    Update changelog.json

--- main.c
+++ main.c
@@ -11,4 +11,4 @@ typedef struct airplaneT {
 int comparePlanes(void *p1, void *p2) {
 	Airplane *a = (Airplane *)p1;
 	Airplane *b = (Airplane *)p2;
-	int timeA = (a->departHr)*6
+	int timeA = (a->departHr)*60 + (a->departMin);
