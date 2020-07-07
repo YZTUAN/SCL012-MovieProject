d84264b027b45b0150add8108270a7f0b458cf33
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2020-07-07 05:58:25 +0000

    Fixed a couple of tests

--- main.c
+++ main.c
@@ -44,4 +44,5 @@ int compareStrings(void *p1, void *p2)
 
 //Destructor functions
 void destroyBasicTypeAlloc(void *p){
-//	//For pointers to basic data types (int*,char*,double*,
+//	//For pointers to basic data types (int*,char*,double*,...)
+//		//Use 
