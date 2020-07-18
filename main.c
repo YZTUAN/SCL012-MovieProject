3d98d011efd5bb5af8680556c09d2b551e91f4f9
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2020-07-18 13:05:04 +0000

    Corrected spelling mistakes

--- main.c
+++ main.c
@@ -45,4 +45,4 @@ int compareStrings(void *p1, void *p2)
 //Destructor functions
 void destroyBasicTypeAlloc(void *p){
 //	//For pointers to basic data types (int*,char*,double*,...)
-//		//Use for allocated memory (m
+//		//Use for allocated memory (malloc,calloc,
