529ada4a533e4253ede82a2a27120ecc36a9989a
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2021-01-28 10:53:23 +0000

    Some things

--- main.c
+++ main.c
@@ -96,4 +96,5 @@ void testWithInts() {
 	printf("Third node : %d\n",(SLNextItem(SLIter) == NULL?0:*((int *)SLGetItem(SLIter))));
 	printf("Fourth node : %d\n",(SLNextItem(SLIter) == NULL?0:*((int *)SLGetItem(SLIter))));
 	printf("Fifth node : %d\n",(SLNextItem(SLIter) == NULL?0:*((int *)SLGetItem(SLIter))));
-	printf("Sixth node : %d\n",(SLNextItem(SLIter) == NULL?0:*((int *)SLGetItem(
+	printf("Sixth node : %d\n",(SLNextItem(SLIter) == NULL?0:*((int *)SLGetItem(SLIter))));
+	p
