8997e86d2b8d971678be76fae3e06c5b9d29f3f1
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2021-02-27 11:00:11 +0000

    SVN: merge trunk-3-5 to trunk-4-0

--- main.c
+++ main.c
@@ -100,4 +100,5 @@ void testWithInts() {
 	printf("Seventh node : %d\n",(SLNextItem(SLIter) == NULL?0:*((int *)SLGetItem(SLIter))));
 	printf("Eighth node : %d\n",(SLNextItem(SLIter) == NULL?0:*((int *)SLGetItem(SLIter))));
 	printf("Ninth node : %d\n",(SLNextItem(SLIter) == NULL?0:*((int *)SLGetItem(SLIter))));
-	printf("Tenth node : %d\n",(SLNextItem(SLIter) == NULL?0:*((int *)SLGetItem(SLIter
+	printf("Tenth node : %d\n",(SLNextItem(SLIter) == NULL?0:*((int *)SLGetItem(SLIter))));
+	printf
