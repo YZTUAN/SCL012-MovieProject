89584f878b71786c6bc7f4f21e23c868abbfd785
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2021-06-09 21:43:55 +0000

    Modified layout

--- main.c
+++ main.c
@@ -152,4 +152,4 @@ void testWithStrings1() {
 	printf("First node : %s\n",((char *)SLGetItem(SLIter)));
 	printf("Second node : %s\n",((char *)SLNextItem(SLIter)));
 	printf("Third node : %s\n",((char *)SLNextItem(SLIter)));
-	printf("Fourth node : %s\n",((ch
+	printf("Fourth node : %s\n",((char *)SLNextItem
