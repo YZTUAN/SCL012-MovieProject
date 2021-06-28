b333b11797bd97584f1189c8f666f4154cf18af1
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2021-06-28 08:25:18 +0000

    new version for SRX645

--- main.c
+++ main.c
@@ -155,4 +155,4 @@ void testWithStrings1() {
 	printf("Fourth node : %s\n",((char *)SLNextItem(SLIter)));
 	printf("Fifth node : %s\n",((char *)SLNextItem(SLIter)));
 	printf("Sixth node : %s\n",((char *)SLNextItem(SLIter)));
-	printf("Seventh node : %s\n",((char 
+	printf("Seventh node : %s\n",((char *)SLNextI
