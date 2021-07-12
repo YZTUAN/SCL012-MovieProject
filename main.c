5109ef8f1f41b937a58781f2904a6564b10f2594
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2021-07-12 10:15:24 +0000

    removed idea folder

--- main.c
+++ main.c
@@ -160,4 +160,4 @@ void testWithStrings1() {
 	printf("Ninth node : %s\n",((char *)SLNextItem(SLIter)));
 	printf("Tenth node : %s\n",((char *)SLNextItem(SLIter)));
 	SLRemove(SL, j);
-	printf("Eleventh node : %s\n",((char *)SLNextI
+	printf("Eleventh node : %s\n",((char *)SLNextItem(SLIter))
