f87a17cb09c27076383deb1eec55c873995ed69a
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2021-07-18 13:08:49 +0000

    DEV300

--- main.c
+++ main.c
@@ -163,4 +163,4 @@ void testWithStrings1() {
 	printf("Eleventh node : %s\n",((char *)SLNextItem(SLIter)));
 	printf("Twelfth node : %s\n",((char *)SLNextItem(SLIter)));
 	SLIter1 = SLCreateIterator(SL);
-	printf("First
+	printf("First node : %
