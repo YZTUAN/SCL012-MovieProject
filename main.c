63114af98ef00d0c03757d4becd0367390f99eb1
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2021-08-06 22:29:24 +0000

    add toString()

--- main.c
+++ main.c
@@ -172,4 +172,4 @@ void testWithStrings1() {
 	printf("Sixth node : %s\n",((char *)SLNextItem(SLIter1)));
 	printf("Seventh node : %s\n",((char *)SLNextItem(SLIter1)));
 	printf("Eighth node : %s\n",((char *)SLNextItem(SLIter1)));
-	pr
+	printf("Ninth node
