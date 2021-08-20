85b8b67b9013d58c9b25278af088067778a0a5db
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2021-08-20 13:17:27 +0000

    Added link to README

--- main.c
+++ main.c
@@ -176,4 +176,4 @@ void testWithStrings1() {
 	printf("Tenth node : %s\n",((char *)SLNextItem(SLIter1)));
 	printf("Eleventh node : %s\n",((char *)SLNextItem(SLIter1)));
 	printf("Twelfth node : %s\n",((char *)SLNextItem(SLIter1)));
-	pr
+	printf("Twelfth
