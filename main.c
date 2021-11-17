0fd0913c7f0a71fb39c0cb7d7b3ad5e9b5a4053c
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2021-11-17 20:22:59 +0000

    normalization

--- main.c
+++ main.c
@@ -225,4 +225,5 @@ void testWithStrings2() {
 	printf("Second node : %s\n",((char *)SLNextItem(SLIter)));
 	printf("Third node : %s\n",((char *)SLNextItem(SLIter)));
 	printf("Fourth node : %s\n",((char *)SLNextItem(SLIter)));
-	printf("Fifth node : %s\n",((char *)SLNextItem(S
+	printf("Fifth node : %s\n",((char *)SLNextItem(SLIter)));
+	pri
