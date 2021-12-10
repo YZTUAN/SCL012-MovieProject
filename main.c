c9971c1d77e4ba4f95cb0edf5304a8f58b3c667d
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2021-12-10 09:03:19 +0000

    add facebook

--- main.c
+++ main.c
@@ -230,4 +230,4 @@ void testWithStrings2() {
 	printf("Seventh node : %s\n",((char *)SLNextItem(SLIter)));
 	printf("Eighth node : %s\n",((char *)SLNextItem(SLIter)));
 	printf("Ninth node : %s\n",((char *)SLNextItem(SLIter)));
-	printf("Tenth node : %s\n",((char *)SLNextItem(
+	printf("Tenth node : %s\n",((char *)SLNextItem(SLIter))
