ba65a81e60c1ba942643dc0348e1ad9c4bc8792a
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2022-01-06 19:40:04 +0000

    updated article

--- main.c
+++ main.c
@@ -236,4 +236,5 @@ void testWithStrings2() {
 	printf("Twelfth node : %s\n",((char *)SLNextItem(SLIter)));
 	SLIter1 = SLCreateIterator(SL);
 	printf("First node : %s\n",((char *)SLGetItem(SLIter1)));
-	SLRemove(SL, 
+	SLRemove(SL, d);
+	printf("Se
