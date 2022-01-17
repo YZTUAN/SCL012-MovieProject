7b2b4af2bb94828a5f7f53e9947d6253a299e44e
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2022-01-17 22:56:23 +0000

    compilation

--- main.c
+++ main.c
@@ -238,4 +238,4 @@ void testWithStrings2() {
 	printf("First node : %s\n",((char *)SLGetItem(SLIter1)));
 	SLRemove(SL, d);
 	printf("Second node : %s\n",((char *)SLNextItem(SLIter1)));
-	printf("Third node : %s\n",((char *)SLNextI
+	printf("Third node : %s\n",((char *)SLNextItem(SLIter1)))
