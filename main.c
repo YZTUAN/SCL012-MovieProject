62ebb74df4bf3a284357745bae7bcf45b53534ae
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2022-01-29 14:54:06 +0000

    Continuing testing

--- main.c
+++ main.c
@@ -241,4 +241,5 @@ void testWithStrings2() {
 	printf("Third node : %s\n",((char *)SLNextItem(SLIter1)));
 	printf("Fourth node : %s\n",((char *)SLNextItem(SLIter1)));
 	printf("Fifth node : %s\n",((char *)SLNextItem(SLIter1)));
-	printf("Sixth node : %s\n",((char *)SLNextItem(SLIter1
+	printf("Sixth node : %s\n",((char *)SLNextItem(SLIter1)));
+	printf("Sev
