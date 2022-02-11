c6e091356bd1f995806a99a3e3b9c5a06600b78b
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2022-02-11 14:10:46 +0000

    Release v0.11.0

--- main.c
+++ main.c
@@ -242,4 +242,4 @@ void testWithStrings2() {
 	printf("Fourth node : %s\n",((char *)SLNextItem(SLIter1)));
 	printf("Fifth node : %s\n",((char *)SLNextItem(SLIter1)));
 	printf("Sixth node : %s\n",((char *)SLNextItem(SLIter1)));
-	printf("Sev
+	printf("Seventh node : 
