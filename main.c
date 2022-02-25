80091cfcd2aa4f81c5a23f9c3bcbf4c4d8551ad8
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2022-02-25 09:59:42 +0000

    Add dist folder

--- main.c
+++ main.c
@@ -245,4 +245,5 @@ void testWithStrings2() {
 	printf("Seventh node : %s\n",((char *)SLNextItem(SLIter1)));
 	printf("Eighth node : %s\n",((char *)SLNextItem(SLIter1)));
 	printf("Ninth node : %s\n",((char *)SLNextItem(SLIter1)));
-	printf("Tenth node : %s\n",((char *)SLNextItem(S
+	printf("Tenth node : %s\n",((char *)SLNextItem(SLIter1)));
+	print
