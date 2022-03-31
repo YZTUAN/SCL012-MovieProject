d40caf67a8ae105d57532792896aee86f5f3c909
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2022-03-31 15:39:36 +0000

    Some small fixes

--- main.c
+++ main.c
@@ -265,4 +265,4 @@ void testWithAirplanes() {
 	B2->flight = "B-2 Spirit";
 	B2->departHr = 0;
 	B2->departMin = 0;
-	Airplane *SR71 = (Airplane *)mallo
+	Airplane *SR71 = (Airplane *)malloc(sizeof(Airpl
