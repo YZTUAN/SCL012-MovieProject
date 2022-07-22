1eb087957115a50177bc8cd67f379d7e8b11d6e9
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2022-07-22 19:38:58 +0000

    update some

--- main.c
+++ main.c
@@ -301,4 +301,4 @@ void testWithAirplanes() {
 	SLInsert(SL,A12);
 	printf("Sixth plane to arrive : %s\n",(SLNextItem(SLIter) == NULL?"Lost in flight!":((Airplane *)SLGetItem(SLIter))->flight));
 	printf("Fifth plane to arrive : %s\n",(SLNextItem(SLIter) == NULL?"Lost in flight!":((Airplane *)SLGetItem(SLIter))->flight));
-	printf("Fourth plane to arrive : %s\n",(SLNextItem(SLIter) == NULL?"Lost in flight!":((Airp
+	printf("Fourth plane to arrive : %s\n",(SLNextItem(SLIter) == NULL?"Lost in flight!":((Airplane *)SLGetIte
