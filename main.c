097d8f9a68717e1d89ce5cbc07e560e04c8fa647
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2022-09-18 11:13:42 +0000

    Fix formatting of README

--- main.c
+++ main.c
@@ -304,4 +304,4 @@ void testWithAirplanes() {
 	printf("Fourth plane to arrive : %s\n",(SLNextItem(SLIter) == NULL?"Lost in flight!":((Airplane *)SLGetItem(SLIter))->flight));
 	printf("Third plane to arrive : %s\n",(SLNextItem(SLIter) == NULL?"Lost in flight!":((Airplane *)SLGetItem(SLIter))->flight));
 	printf("Second plane to arrive : %s\n",(SLNextItem(SLIter) == NULL?"Lost in flight!":((Airplane *)SLGetItem(SLIter))->flight));
-	printf("First plane to arrive : %s\n",(SLNextItem(SLIter) == 
+	printf("First plane to arrive : %s\n",(SLNextItem(SLIter) == NULL?"Lost in fl
