2667bd38d242b4fb302d8060ca90c49d47e6066f
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2022-10-09 07:44:12 +0000

    Fix missing dependencies

--- main.c
+++ main.c
@@ -308,4 +308,4 @@ void testWithAirplanes() {
 	
 	printf("Day 2:\n");
 	SLIter1 = SLCreateIterator(SL);
-	printf("Last plane to arrive : %s\n",(SLGetItem(
+	printf("Last plane to arrive : %s\n",(SLGetItem(SLIter1) == 
