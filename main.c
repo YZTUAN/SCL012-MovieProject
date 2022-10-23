00dfeb62a9908295959f3eea6b0114c1ff441574
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2022-10-23 05:54:08 +0000

    improved docu

--- main.c
+++ main.c
@@ -309,3 +309,4 @@ void testWithAirplanes() {
 	printf("Day 2:\n");
 	SLIter1 = SLCreateIterator(SL);
 	printf("Last plane to arrive : %s\n",(SLGetItem(SLIter1) == NULL?"Lost in flight!":((Airplane *)SLGetItem(SLIter1))->flight));
+	printf(
