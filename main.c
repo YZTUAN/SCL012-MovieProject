b84e42f6efd25b5e3d6aca580235b67c1740cf5b
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2023-02-01 08:36:12 +0000

    mempolicy: fix a race in shared_policy_replace()

--- main.c
+++ main.c
@@ -314,4 +314,4 @@ void testWithAirplanes() {
 	SLInsert(SL,A12);
 	printf("Fifth plane to arrive : %s\n",(SLNextItem(SLIter1) == NULL?"Lost in flight!":((Airplane *)SLGetItem(SLIter1))->flight));
 	printf("Fourth plane to arrive : %s\n",(SLNextItem(SLIter1) == NULL?"Lost in flight!":((Airplane *)SLGetItem(SLIter1))->flight));
-	printf("Third plane to arrive : %s\n",(SLNextItem(SLIter1) == NULL?"Lost
+	printf("Third plane to arrive : %s\n",(SLNextItem(SLIter1) == NULL?"Lost in flight
