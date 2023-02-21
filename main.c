4ff7b4a714b10aca811ec537cbc66f5afa2d2184
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2023-02-21 05:51:20 +0000

    minor grammar fix

--- main.c
+++ main.c
@@ -318,3 +318,4 @@ void testWithAirplanes() {
 	printf("Second plane to arrive : %s\n",(SLNextItem(SLIter1) == NULL?"Lost in flight!":((Airplane *)SLGetItem(SLIter1))->flight));
 	printf("First plane to arrive : %s\n",(SLNextItem(SLIter1) == NULL?"Lost in flight!":((Airplane *)SLGetItem(SLIter1))->flight));
 	
+	SLDestroy(SL);
