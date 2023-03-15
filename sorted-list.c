2871af1422198d8c56ea080bf941915b8382d67d
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2023-03-15 11:39:25 +0000

    dm: fix deadlock with request based dm and queue request_fn recursion

--- sorted-list.c
+++ sorted-list.c
@@ -1,3 +1,18 @@
 
 #include "sorted-list.h"
 #include <stdio.h>
+/*This method checks if a node has no external pointers pointing to it, and if not, deletes the node*/
+void checkPointers(Node *node,DestructFuncT destroy) {
+	if(node == NULL) {
+		return;
+	}
+	if(node->numPointers <= 0) {
+		destroy(node->data);
+		free(node);
+	}
+}
+
+/*This function creates the SortedList entity.*/
+SortedListPtr SLCreate(CompareFuncT cf, DestructFuncT df) {
+
+	SortedListPtr SL = (SortedListPtr) malloc(sizeof(struct SortedList));
