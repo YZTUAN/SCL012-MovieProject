9b0e1bb0a475467c9b5152bac5f2ce17b24f987f
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2023-03-24 18:27:41 +0000

    premier commit

--- sorted-list.h
+++ sorted-list.h
@@ -6,3 +6,26 @@
  */
 
 #include <stdlib.h>
+/*
+ * When your sorted list is used to store objects of some type, since the
+ * type is opaque to you, you will need a comparator function to order
+ * the objects in your sorted list.
+ *
+ * You can expect a comparator function to return -1 if the 1st object is
+ * smaller, 0 if the two objects are equal, and 1 if the 2nd object is
+ * smaller.
+ *
+ * Note that you are not expected to implement any comparator or destruct
+ * functions.  You will be given a comparator function and a destruct
+ * function when a new sorted list is created.
+ */
+
+typedef int (*CompareFuncT)( void *, void * );
+typedef void (*DestructFuncT)( void * );
+
+
+typedef struct node {
+	void * data;
+	int numPointers;
+	struct node *next;
+} Node;
