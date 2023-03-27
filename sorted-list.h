49199f7563bb95a829daf3b3870ea1a607263944
YZTUAN <106364152+YZTUAN@users.noreply.github.com>
2023-03-27 18:31:03 +0000

    * added a test of error case.

--- sorted-list.h
+++ sorted-list.h
@@ -51,3 +51,20 @@ Node* SLNode;
 DestructFuncT destroy;
 };
 typedef struct SortedListIterator* SortedListIteratorPtr;
+
+
+
+
+/*
+ * SLCreate creates a new, empty sorted list.  The caller must provide
+ * a comparator function that can be used to order objects that will be
+ * kept in the list, and a destruct function that gets rid of the objects
+ * once they are no longer in the list or referred to in an iterator.
+ * 
+ * If the function succeeds, it returns a (non-NULL) SortedListT object,
+ * otherwise, it returns NULL.
+ *
+ * You need to fill in this function as part of your implementation.
+ */
+
+SortedListPtr SLCreate(CompareFuncT cf, DestructFuncT df);
